85f7ddda147300bacb4b3a488bee81f57a624f46
ELderblood <87793589+ReekElderblood@users.noreply.github.com>
2024-05-25 19:21:13 +0530

    Update monitormax.sh
    The latest version of SysMon Pro introduces an enhanced menu system, designed to improve usability and streamline access to key functionalities. With the new menu, users can quickly navigate through the different monitoring options, including Disk Usage, CPU Load, RAM Usage, Kernel Version, and Check All. This menu enhancement enhances the user experience, making SysMon Pro even more efficient and user-friendly.

--- monitormax.sh
+++ monitormax.sh
@@ -1,39 +1,69 @@
 #!/bin/bash
 
 # Define colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-NC='\033[0m' # No Color
+RED='\033[0;91m'    # Red
+GREEN='\033[0;92m'  # Green
+YELLOW='\033[0;93m' # Yellow
+BLUE='\033[0;94m'   # Blue
+CYAN='\033[0;96m'   # Cyan
+MAGENTA='\033[0;95m' # Magenta
+NC='\033[0m'        # No Color
 
 # Function to display disk usage
 disk_usage() {
+    echo -e "${BLUE}==================== Disk Usage ====================${NC}\n"
     echo -e "${GREEN}$(df -h)${NC}"
 }
 
 # Function to display CPU usage
 cpu_usage() {
+    echo -e "\n${CYAN}==================== CPU Usage =====================${NC}\n"
     echo -e "${YELLOW}$(top -bn1 | grep "Cpu(s)")${NC}"
 }
 
 # Function to display RAM usage
 ram_usage() {
-    echo -e "${CYAN}$(free -h)${NC}"
+    echo -e "\n${GREEN}==================== RAM Usage =====================${NC}\n"
+    echo -e "${MAGENTA}$(free -h)${NC}"
 }
 
 # Function to display the kernel version
 kernel_name() {
-    echo -e "Kernel Versions :" "${MAGENTA}$(uname -r)${NC}"
+    echo -e "\n${YELLOW}==================== Kernel Version ==================${NC}\n"
+    echo -e "${BLUE}Kernel Versions : $(uname -r)${NC}"
+    echo -e "\n"
 }
-echo -e "${BLUE}==================== Disk Usage ====================${NC}\n"
-disk_usage
-echo -e "\n${BLUE}==================== CPU Usage =====================${NC}\n"
-cpu_usage
-echo -e "\n${BLUE}==================== RAM Usage =====================${NC}\n"
-ram_usage
-echo -e "\n${BLUE}==================== Kernel Version ==================${NC}\n"
-kernel_name
-echo -e "\n"
+
+# Function to check all information
+check_all() {
+    disk_usage
+    cpu_usage
+    ram_usage
+    kernel_name
+}
+
+# Function to display the menu
+menu() {
+    echo -ne "
+
+${BLUE}1) Disk Usage
+${CYAN}2) CPU Load
+${GREEN}3) RAM Usage
+${YELLOW}4) Kernel Version
+${MAGENTA}5) Check All
+${RED}0) Exit${NC}
+"
+    read -r -p "Choose an option: " choice
+    case $choice in
+        1) disk_usage ; menu;;
+        2) cpu_usage ; menu;;
+        3) ram_usage ; menu;;
+        4) kernel_name ; menu;;
+        5) check_all ; menu;;
+        0) exit 0 ;;
+        *) echo -e "${RED}Wrong option.${NC}";;
+    esac
+}
+
+# Initial call to the menu function
+menu
